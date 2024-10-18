b883a2eaff7074d19cce70c750c3f6b5d28df8c0
Kivlanizzu <kivlanizzudin@gmail.com>
2024-10-18 21:55:20 +0700

    penambahan getlocation pada js

--- index.js
+++ index.js
@@ -11,26 +11,41 @@ function dateFormatted(date) {
     return dateFormatted.toLocaleDateString("id-ID", options);
 }
 
-const dailyTemp = "https://api.open-meteo.com/v1/forecast?latitude=-6.1818&longitude=106.8223&daily=weather_code,temperature_2m_max,temperature_2m_min"
 
 
-async function getWeather() {
+async function getWeather(latitude,longitude) {
     try {
-        const response = await fetch(dailyTemp);
+        const response = await fetch( `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min`);
         const data = await response.json();
-        data.daily.time.forEach((date, index) => {
+        for (let i = 0; i < data.daily.time.length; i++) {
             document.getElementById("days").innerHTML += 
-            `<div class="col-12 col-md-3">
+            `<div class="col-12 col-md-4">
                 <div class="card text-bg-light mb-3">
                     <div class="card-body">
-                        <h5 class="card-title">${dateFormatted(date)}</h5>
-                        <p class="card-text">${daily.temperature_2m_min[index]} - ${daily.temperature_2m_max[index]}</p>
+                        <h5 class="card-title">${dateFormatted(data.daily.time[i])}</h5>
+                        <p class="card-text">${data.daily.temperature_2m_min[i]} - ${data.daily.temperature_2m_max[i]}</p>
                     </div>
                 </div>
             </div>`
-        })
+        }
     } catch (error) {
         console.log(error);
     }
 }
-getWeather();
+
+
+function showPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    getWeather(latitude,longitude);
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+getLocation()
+
+const currentweather = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,is_day,weather_code&timezone=auto"

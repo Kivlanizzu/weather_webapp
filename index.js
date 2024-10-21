0b49ae3b3f3601c84db1a367a503c170b31785e4
Kivlanizzu <kivlanizzudin@gmail.com>
2024-10-21 22:18:00 +0700

    mengubah tampilan css search, font dan background

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const d = new Date(); 
-document.getElementById("date").innerHTML = d;
 function dateFormatted(date) {
     const options = {
         weekday: "long",
@@ -8,10 +6,22 @@ function dateFormatted(date) {
         day: "numeric",
     };
     const dateFormatted = new Date(date);
-    return dateFormatted.toLocaleDateString("id-ID", options);
+    return dateFormatted.toLocaleDateString("en-US", options);
 }
 
-
+async function getWeatherByCity() {
+    const city = document.getElementById("city").value;
+    const response = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`
+    );
+    const responseJson = await response.json();
+    const latitude = responseJson.results[0].latitude;
+    const longitude = responseJson.results[0].longitude;
+    const cityName = responseJson.results[0].name;
+    getWeather(latitude, longitude);
+    document.getElementById("city-name").innerHTML = cityName;
+    document.getElementById("city").value = "";
+}
 
 async function getWeather(latitude,longitude) {
     try {
@@ -19,23 +29,24 @@ async function getWeather(latitude,longitude) {
         const data = await response.json();
         const response2 = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,weather_code`);
         const data2 = await response2.json();
+        document.getElementById("days").innerHTML = "";
         for (let i = 1; i < data.daily.time.length; i++) {
             document.getElementById("days").innerHTML += 
             `<div class="col-12 col-md-4">
-                <div class="card text-bg-light mb-3">
+                <div class="card text-bg-light mb-3" stryle="background-color: 586990;">
                     <div class="card-body">
                         <h5 class="card-title">${dateFormatted(data.daily.time[i])}</h5>
-                        <p class="card-text">${data.daily.temperature_2m_min[i]} C - ${data.daily.temperature_2m_max[i]} C</p>
+                        <p class="card-text">${data.daily.temperature_2m_min[i]} °C ~ ${data.daily.temperature_2m_max[i]} °C</p>
                     </div>
                 </div>
             </div>`
         };
         document.getElementById("currenttemp").innerHTML = 
-        `<p>${data2.current.temperature_2m} C</p>`;
-        document.getElementById("city").innerHTML = 
-        `<p>${data2.timezone} </p>`;
-
-    } catch (error) {
+        `<p>${data2.current.temperature_2m} °C</p>`;
+        document.getElementById("date").innerHTML = 
+        `<p>${dateFormatted(data2.current.time)}</p>`;
+    } 
+    catch (error) {
         console.log(error);
     }
 }
